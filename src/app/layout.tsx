94a1223b9f8eff021aefb70729b2bc9818bd5050
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2023-11-19 09:52:52 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <link rel="shortcut icon" href={config.site.favicon} />
         {/* theme meta */}
         <meta name="theme-name" content="nextplate" />
-        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileColor" content="#000009" />
         <meta
           name="theme-color"
           media="(prefers-color-scheme: light)"
